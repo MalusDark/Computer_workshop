a37b2a9d2cdbe2c989523a77d0e35bd973009bb4
Malus <artem.kotegov@mail.ru>
2023-10-10 13:05:05 +0300

    Добавлена тестовая основная страница

--- computer_workshop/app/Providers/RouteServiceProvider.php
+++ computer_workshop/app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
+    protected $namespace = 'App\Http\Controllers';
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
@@ -29,12 +30,13 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
             Route::middleware('web')
+                ->namespace($this->namespace)  // добавьте эту строку
                 ->group(base_path('routes/web.php'));
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)  // добавьте эту строку
+                ->group(base_path('routes/api.php'));
         });
     }
 }
